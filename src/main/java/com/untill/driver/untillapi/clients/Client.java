834e6784ba6d8df26c68fe45827c05bb712c65d6
michael-say <smmbox@gmail.com>
2018-07-25 12:40:04 +0300

    Clients API added

---
+++ src/main/java/com/untill/driver/untillapi/clients/Client.java
@@ -0,0 +1,177 @@
+package com.untill.driver.untillapi.clients;
+
+public class Client {
+
+	private Long id;
+	
+	private String name;
+	
+	private Integer number;
+	
+	private String reference;
+	
+	private Long birthday;
+
+	private String email;
+	
+	private String phone;
+	
+	private String fax;
+	
+	private String website;
+	
+	private Long countryId;
+		
+	private String zip;
+	
+	private String city;
+	
+	private String address;
+	
+	private String info;
+	
+	private Boolean onInvoice;
+	
+	private Long lastUpdate;
+	
+	private Boolean active;
+
+	public Boolean isActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+	public Long getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Long birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public Long getCountryId() {
+		return countryId;
+	}
+
+	public void setCountryId(Long countryId) {
+		this.countryId = countryId;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public Boolean getOnInvoice() {
+		return onInvoice;
+	}
+
+	public void setOnInvoice(Boolean onInvoice) {
+		this.onInvoice = onInvoice;
+	}
+
+	public Long getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(Long lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/untill/driver/untillapi/clients/IUntillClientsApi.java
@@ -0,0 +1,26 @@
+package com.untill.driver.untillapi.clients;
+
+import java.util.List;
+
+import com.untill.driver.IDriverContext;
+import com.untill.driver.untillapi.IUntillApi;
+import com.untill.driver.untillapi.countries.Country;
+
+/**
+ * UntillAPI for working with Clients
+ * @see IUntillApi
+ * @see IDriverContext
+ */
+public interface IUntillClientsApi extends IUntillApi {
+	/**
+	 * @return Returns list of clients in database
+	 */
+	public List<Client> getClients();
+	
+	/**
+	 * Inserts or updates client in database. When "id" is null, item inserted, otherwise updated
+	 * @param item
+	 */
+	public void saveClient(Client item);
+
+}
