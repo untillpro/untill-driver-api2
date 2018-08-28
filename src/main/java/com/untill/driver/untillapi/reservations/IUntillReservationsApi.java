87de2eac7f23b1263bfe962b4a9597dce85f0620
michael-say <smmbox@gmail.com>
2018-08-28 14:56:39 +0300

    Reservation API update

--- src/main/java/com/untill/driver/untillapi/reservations/IUntillReservationsApi.java
+++ src/main/java/com/untill/driver/untillapi/reservations/IUntillReservationsApi.java
@@ -7,21 +7,36 @@ import com.untill.driver.untillapi.IUntillApi;
 
 /**
  * UntillAPI for working with Reservations
+ * 
  * @see IUntillApi
  * @see IDriverContext
  */
 public interface IUntillReservationsApi extends IUntillApi {
-	
+
 	/**
 	 * @return Returns reservations by reference
 	 */
 	public List<Reservation> getReservations(String reference);
-	
+
 	/**
-	 * Inserts or updates reservation in database. When "id" is null, item inserted, otherwise updated
-	 * @param item Reservation to insert or update
+	 * Inserts or updates reservation in database. When "id" is null, item
+	 * inserted, otherwise updated
+	 * 
+	 * @param item
+	 *            Reservation to insert or update
 	 * @return Returns item ID
 	 */
 	public Long saveReservation(Reservation item);
+
+	/**
+	 * Restuns list of reservations
+	 * 
+	 * @param updatedSince
+	 *            when null, method returns only active reservations (status =
+	 *            NOT_ACCEPTED or ACCEPTED). When updatedSince is not null, it
+	 *            returns all reservations updated since specified time
+	 * @return List of reservations
+	 */
+	public List<Reservation> getReservations(Long updatedSince);
 
 }
