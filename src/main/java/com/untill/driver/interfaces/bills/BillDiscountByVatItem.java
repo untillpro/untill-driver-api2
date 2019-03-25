023243ac28d5b111873cb4486aed575f136ed407
michael-say <smmbox@gmail.com>
2019-03-25 15:12:17 +0300

    DriverAPI updates https://dev.untill.com/projects/#!514285

---
+++ src/main/java/com/untill/driver/interfaces/bills/BillDiscountByVatItem.java
@@ -0,0 +1,48 @@
+package com.untill.driver.interfaces.bills;
+
+import java.math.BigDecimal;
+
+public class BillDiscountByVatItem extends BillDiscountItem {
+
+	private BigDecimal vatPercent;
+
+	private String vatSign;
+	
+	private String discountKey;
+	
+	private String discountTitle;
+	
+	public String getDiscountKey() {
+		return discountKey;
+	}
+
+	public void setDiscountKey(String discountKey) {
+		this.discountKey = discountKey;
+	}
+
+	public String getDiscountTitle() {
+		return discountTitle;
+	}
+
+	public void setDiscountTitle(String discountTitle) {
+		this.discountTitle = discountTitle;
+	}
+
+	public BigDecimal getVatPercent() {
+		return vatPercent;
+	}
+
+	public void setVatPercent(BigDecimal vatPercent) {
+		this.vatPercent = vatPercent;
+	}
+
+	public String getVatSign() {
+		return vatSign;
+	}
+
+	public void setVatSign(String vatSign) {
+		this.vatSign = vatSign;
+	}
+
+
+}
