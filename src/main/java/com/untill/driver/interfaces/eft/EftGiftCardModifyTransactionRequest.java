a8f7562c33d4c33f9905f3294614bf0dceb00b5d
michael-say <smmbox@gmail.com>
2019-05-30 12:52:12 +0300

    timestamp added in request

--- src/main/java/com/untill/driver/interfaces/eft/EftGiftCardModifyTransactionRequest.java
+++ src/main/java/com/untill/driver/interfaces/eft/EftGiftCardModifyTransactionRequest.java
@@ -1,11 +1,22 @@
 package com.untill.driver.interfaces.eft;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class EftGiftCardModifyTransactionRequest extends EftRequest {
 
 	private Map<String, String> data = new HashMap<>();
+
+	private Date timestamp;
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
 
 	public Map<String, String> getData() {
 		return data;
