93e3ce644c4fb16b7f35432e280a1dd837964158
michael-say <smmbox@gmail.com>
2019-07-11 17:04:13 +0300

    Javadoc

--- src/main/java/com/untill/driver/interfaces/eft/EftGiftCardPaymentRequest.java
+++ src/main/java/com/untill/driver/interfaces/eft/EftGiftCardPaymentRequest.java
@@ -4,8 +4,8 @@ package com.untill.driver.interfaces.eft;
  * Gift Card Payment Request.
  * Provides amount to withdraw from gift card. 
  * Driver responds with {@link EftResult} or throws RuntimeException. 
- * <br/>Current card balance may returned in {@link EftResult#getData()} under {@link EftResultFields#CURRENT_BALANCE} key in cents.
- * <br/>Old card balance may be returned in {@link EftResult#getData()} under {@link EftResultFields#OLD_BALANCE} key in cents.  
+ * <br>Current card balance may returned in {@link EftResult#getData()} under {@link EftResultFields#CURRENT_BALANCE} key in cents.
+ * <br>Old card balance may be returned in {@link EftResult#getData()} under {@link EftResultFields#OLD_BALANCE} key in cents.  
  * @see IEft
  */
 public class EftGiftCardPaymentRequest extends EftGiftCardValueTransactionRequest {
--- src/main/java/com/untill/driver/interfaces/eft/EftGiftCardReloadRequest.java
+++ src/main/java/com/untill/driver/interfaces/eft/EftGiftCardReloadRequest.java
@@ -2,9 +2,9 @@ package com.untill.driver.interfaces.eft;
 
 /**
  * Gift Card Reload Request - called to increase card balance by certain amount.
- * <br/>Driver responds with {@link EftResult} or throws RuntimeException.
- * <br/>Current card balance returned in {@link EftResult#getData()} under {@link EftResultFields#CURRENT_BALANCE} key in cents.
- * <br/>Old card balance may be returned in {@link EftResult#getData()} under {@link EftResultFields#OLD_BALANCE} key in cents.  
+ * <br>Driver responds with {@link EftResult} or throws RuntimeException.
+ * <br>Current card balance returned in {@link EftResult#getData()} under {@link EftResultFields#CURRENT_BALANCE} key in cents.
+ * <br>Old card balance may be returned in {@link EftResult#getData()} under {@link EftResultFields#OLD_BALANCE} key in cents.  
  * @see IEft
  */
 public class EftGiftCardReloadRequest extends EftGiftCardValueTransactionRequest {
