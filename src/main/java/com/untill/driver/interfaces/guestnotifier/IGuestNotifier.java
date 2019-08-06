7fccce3d835994f2e7e95b31f79159df619fe9eb
michael-say <smmbox@gmail.com>
2019-08-06 17:47:37 +0300

    DriverConfiguration argument added

--- src/main/java/com/untill/driver/interfaces/guestnotifier/IGuestNotifier.java
+++ src/main/java/com/untill/driver/interfaces/guestnotifier/IGuestNotifier.java
@@ -2,6 +2,7 @@ package com.untill.driver.interfaces.guestnotifier;
 
 import com.untill.driver.IDriver;
 import com.untill.driver.interfaces.IDriverInterface;
+import com.untill.driver.params.DriverConfiguration;
 
 /**
  * Driver interface for working with so called "Guest Notifier" hardware: buzzers, pagers, etc
@@ -11,5 +12,5 @@ import com.untill.driver.interfaces.IDriverInterface;
  */
 public interface IGuestNotifier extends IDriverInterface {
 
-	GuestNotifierResult operation(GuestNotifierRequest request);
+	GuestNotifierResult operation(DriverConfiguration cfg, GuestNotifierRequest request);
 }
