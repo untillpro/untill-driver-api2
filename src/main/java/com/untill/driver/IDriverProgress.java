ae3f18685b1aa4378208f8465c127e5b68adaa59
Michael <smmbox@gmail.com>
2019-10-11 15:37:00 +0300

    Queries in driver progress

--- src/main/java/com/untill/driver/IDriverProgress.java
+++ src/main/java/com/untill/driver/IDriverProgress.java
@@ -20,5 +20,21 @@ public interface IDriverProgress {
 	 * @param message Message to show in request's progress dialog
 	 */
 	void showProgressMessage(String requestGuid, String message);
+	
+	/**
+	 * Displays query in unTill POS progress dialog expecting some user input
+	 * @param requestGuid Request GUID
+	 * @param id Query ID. If the same request assumes several queries, different IDs must be used to avoid conflicts
+	 * @param kind Query kind, see {@link ProgressQueryType}
+	 * @param title Title shown in POS
+	 */
+	void showQuery(String requestGuid, int id, ProgressQueryType kind, String title);
+	
+	/**
+	 * @param requestGuid Request GUID
+	 * @param id Query ID
+	 * @return Returns result of a query, or <b>null</b> if result is not yet defined
+	 */
+	String getQueryResult(String requestGuid, int id);
 
 }
---
+++ src/main/java/com/untill/driver/ProgressQueryType.java
@@ -0,0 +1,34 @@
+package com.untill.driver;
+
+/**
+ * Declares progress query kinds
+ * @see {@link IDriverProgress#showQuery}
+ * 
+ */
+public enum ProgressQueryType {
+
+	/**
+	 * A text query with two buttons: YES or NO.  {@link IDriverProgress#getQueryResult} returns "y" or "n"
+	 */
+	YES_NO(1);
+
+	ProgressQueryType(int type) {
+		this.type = type;
+	}
+
+	private int type;
+
+	public int getType() {
+		return type;
+	}
+
+	public static ProgressQueryType getByValue(int i) {
+		for (ProgressQueryType type : ProgressQueryType.values()) {
+			if (type.getType() == i) {
+				return type;
+			}
+		}
+		throw new IllegalArgumentException("There is no value = " + i + " in ProgressQueryType");
+	}
+
+}
