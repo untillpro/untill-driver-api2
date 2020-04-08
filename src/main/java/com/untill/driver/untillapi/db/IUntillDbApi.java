bdfde5a8aa6e6b0551356cc2357000ac424c93d4
sda <d.solovyov@dev.untill.com>
2020-04-08 12:25:52 +0300

    To interface IUntillDbApi added method getName

--- src/main/java/com/untill/driver/untillapi/db/IUntillDbApi.java
+++ src/main/java/com/untill/driver/untillapi/db/IUntillDbApi.java
@@ -5,6 +5,7 @@ import com.untill.driver.untillapi.IUntillApi;
 
 /**
  * UntillAPI for working with Database
+ *
  * @see IUntillApi
  * @see IDriverContext
  */
@@ -14,10 +15,16 @@ public interface IUntillDbApi extends IUntillApi {
 	 * @return Generates and returns the next ID in current database
 	 */
 	Long getNextId();
-	
+
 	/**
 	 * @return Returns GUID of the current database
 	 */
 	String getGuid();
 
+	/**
+	 * Returns name of the current database
+	 *
+	 * @return name of the current database
+	 */
+	String getName();
 }
