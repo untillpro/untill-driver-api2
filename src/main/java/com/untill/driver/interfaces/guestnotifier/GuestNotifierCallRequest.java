467e0b8009e3552f1f68b8c228430c1b1f2f6210
Michael <smmbox@gmail.com>
2021-03-11 17:35:51 +0300

    Custom message added in GuestNotifierCallRequest

--- src/main/java/com/untill/driver/interfaces/guestnotifier/GuestNotifierCallRequest.java
+++ src/main/java/com/untill/driver/interfaces/guestnotifier/GuestNotifierCallRequest.java
@@ -9,6 +9,23 @@ public class GuestNotifierCallRequest extends GuestNotifierRequest {
 	 * Caller identifier
 	 */
 	private String callerId;
+	
+	private String customMessage;
+
+	/**
+	 * @return Returns custom message for Call request
+	 */
+	public String getCustomMessage() {
+		return customMessage;
+	}
+
+	/**
+	 * Sets custom message for Call request
+	 * @param customMessage custom message to set
+	 */
+	public void setCustomMessage(String customMessage) {
+		this.customMessage = customMessage;
+	}
 
 	public String getCallerId() {
 		return callerId;
