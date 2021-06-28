896dfaa6f192b42f67aade9bf03df752a6a1490b
sda <d.solovyov@dev.untill.com>
2021-06-28 17:58:28 +0300

    Added IUntillArticlesApi interface

---
+++ src/main/java/com/untill/driver/untillapi/articles/IUntillArticlesApi.java
@@ -0,0 +1,28 @@
+package com.untill.driver.untillapi.articles;
+
+import com.untill.driver.IDriverContext;
+import com.untill.driver.untillapi.IUntillApi;
+
+import java.util.List;
+
+/**
+ * UntillAPI for working with Articles
+ *
+ * @see IUntillApi
+ * @see IDriverContext
+ */
+public interface IUntillArticlesApi extends IUntillApi {
+	/**
+	 * Returns all available articles
+	 *
+	 * @return All available articles
+	 */
+	List<Article> getAllArticles();
+
+	/**
+	 * Update articles in batch
+	 *
+	 * @param articles list for update
+	 */
+	void batchUpdate(List<Article> articles);
+}
