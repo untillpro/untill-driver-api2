96c20038be198360a14ed6c0f999beae500ee71d
Daniil Solovyov <solovyov.daniil@gmail.com>
2022-04-30 16:42:33 +0300

    Added data map to BillRePrintingRequest (#68)
    Co-authored-by: sda <d.solovyov@dev.untill.com>

--- src/main/java/com/untill/driver/interfaces/bills/BillRePrintingRequest.java
+++ src/main/java/com/untill/driver/interfaces/bills/BillRePrintingRequest.java
@@ -1,11 +1,30 @@
 package com.untill.driver.interfaces.bills;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Class representing a request for "Bill Re-print" operation handled by {@link IBillsHandler}}
- * 
+ *
  * @see IBillsHandler
  * @see BillsHandlerRequest
  */
 public class BillRePrintingRequest extends BillsHandlerRequest {
 
+	private Map<String, String> data = new HashMap<>();
+
+	public Map<String, String> getData() {
+		return data;
+	}
+
+	public void setData(Map<String, String> data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "BillRePrintingRequest{"
+				+ "data=" + data
+				+ "} " + super.toString();
+	}
 }
