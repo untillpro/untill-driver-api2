62185af661289c3ad982ddd00cee982cef58f1ed
Daniil Solovyov <solovyov.daniil@gmail.com>
2022-08-04 16:51:27 +0300

    PLU aliases support (#69)

--- src/main/java/com/untill/driver/untillapi/beveragecontrol/IUntillBeverageControlApi.java
+++ src/main/java/com/untill/driver/untillapi/beveragecontrol/IUntillBeverageControlApi.java
@@ -3,6 +3,8 @@ package com.untill.driver.untillapi.beveragecontrol;
 import com.untill.driver.IDriverContext;
 import com.untill.driver.untillapi.IUntillApi;
 
+import java.util.List;
+
 /**
  * UntillAPI for working with Beverage Control
  *
@@ -16,6 +18,16 @@ public interface IUntillBeverageControlApi extends IUntillApi {
 	 *
 	 * @param registerPouringRequest registration data about beverage pouring
 	 * @return entity ID
+	 * @deprecated This method works incorrectly. Please use {@link IUntillBeverageControlApi#registerPourings(RegisterPouringRequest)}
 	 */
+	@Deprecated
 	Long registerPouring(RegisterPouringRequest registerPouringRequest);
+
+	/**
+	 * Adds records to <tt>VANDUIJNEN</tt> table
+	 *
+	 * @param registerPouringRequest registration data about beverage pouring
+	 * @return list of entities ID
+	 */
+	List<Long> registerPourings(RegisterPouringRequest registerPouringRequest);
 }
