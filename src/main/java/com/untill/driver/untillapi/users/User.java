c48111f85cc255c08c47c86c5e5b895e3e48640a
Daniil Solovyov <solovyov.daniil@gmail.com>
2024-01-23 15:26:46 +0100

    Add fields to User, add field to Bill and add method to IUntillUsersApi (#94)
    https://dev.untill.com/projects/#!676205
    
    Co-authored-by: sda <d.solovyov@dev.untill.com>

--- src/main/java/com/untill/driver/untillapi/users/User.java
+++ src/main/java/com/untill/driver/untillapi/users/User.java
@@ -3,6 +3,9 @@ package com.untill.driver.untillapi.users;
 public class User {
 	private Long id;
 	private String name;
+	private String firstName;
+	private String lastName;
+	private String operatorId;
 
 	public Long getId() {
 		return id;
@@ -20,11 +23,38 @@ public class User {
 		this.name = name;
 	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getOperatorId() {
+		return operatorId;
+	}
+
+	public void setOperatorId(String operatorId) {
+		this.operatorId = operatorId;
+	}
+
 	@Override
 	public String toString() {
 		return "User{"
 				+ "id=" + id
 				+ ", name='" + name + '\''
+				+ ", firstName='" + firstName + '\''
+				+ ", lastName='" + lastName + '\''
+				+ ", operatorId='" + operatorId + '\''
 				+ '}';
 	}
 }
