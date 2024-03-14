9cc415974008117e0e3b41fdf1738d4ee2d72fb8
Daniil Solovyov <solovyov.daniil@gmail.com>
2024-03-14 13:00:23 +0100

    Add hhtIP field to PosPrinterRequest (#102)
    https://dev.untill.com/projects/#!685376
    
    Co-authored-by: sda <d.solovyov@dev.untill.com>
    (cherry picked from commit 31a34cae6b3105feeb8d229c4cedafed07e37be3)

--- src/main/java/com/untill/driver/interfaces/posprinter/PosPrinterRequest.java
+++ src/main/java/com/untill/driver/interfaces/posprinter/PosPrinterRequest.java
@@ -3,8 +3,24 @@ package com.untill.driver.interfaces.posprinter;
 import com.untill.driver.interfaces.DriverRequest;
 
 public abstract class PosPrinterRequest extends DriverRequest {
+	/**
+	 * Handheld terminal IP address. This field is filled when request initiator is handheld terminal
+	 */
+	private String hhtIp;
+
+	public String getHhtIp() {
+		return hhtIp;
+	}
+
+	public void setHhtIp(String hhtIp) {
+		this.hhtIp = hhtIp;
+	}
+
 	@Override
 	public String toString() {
-		return "PosPrinterRequest{} " + super.toString();
+		return "PosPrinterRequest{"
+				+ "hhtIp='" + hhtIp + '\''
+				+ "} "
+				+ super.toString();
 	}
 }
