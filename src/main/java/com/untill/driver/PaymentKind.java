76cecc36089160518241e3aa813448bc5095646a
sda <d.solovyov@dev.untill.com>
2024-07-05 10:00:12 +0200

    Update payment kinds https://dev.untill.com/projects/#!699490

--- src/main/java/com/untill/driver/PaymentKind.java
+++ src/main/java/com/untill/driver/PaymentKind.java
@@ -16,4 +16,9 @@ public final class PaymentKind {
 	public static final int CASHDRO = 9;
 	public static final int NEWYSE_ONACCOUNT = 10;
 	public static final int GIFT_CARD = 11;
+	public static final int PREPAID_CARD_BEVERAGE = 12;
+	public static final int VOUCHER = 13;
+	public static final int ZAPPER = 14;
+	public static final int COUPON = 15;
+	public static final int BILL_DEPOSIT = 16;
 }
