129e5c6a2b8809843e017fbed106cfdd66c8a8e0
Daniil Solovyov <solovyov.daniil@gmail.com>
2025-01-30 18:50:01 +0100

    Add wait timeout to bills handler settings (#123)
    https://dev.untill.com/projects/#!725907
    
    Co-authored-by: sda <d.solovyov@dev.untill.com>

--- src/main/java/com/untill/driver/interfaces/bills/BillsHandlerSettings.java
+++ src/main/java/com/untill/driver/interfaces/bills/BillsHandlerSettings.java
@@ -1,16 +1,14 @@
 package com.untill.driver.interfaces.bills;
 
-import com.untill.driver.interfaces.fiscalprinter.IFiscalPrinter;
-
 /**
  * An instance of this class returned by {@link IBillsHandler#getSettings()} to specify details of driver behavior
  *
- * @see IFiscalPrinter
+ * @see IBillsHandler
  */
 public class BillsHandlerSettings {
-	
-	
-	boolean consolidatedCombiDealsExpected;
+	public static final int DEFAULT_WAIT_TIMEOUT = 50_000;
+	private boolean consolidatedCombiDealsExpected;
+	private int waitTimeout = DEFAULT_WAIT_TIMEOUT;
 
 	/**
 	 * @return true if the driver expects combi-deals to be sent from POS consolidated in the {@link Bill} object
@@ -20,12 +18,30 @@ public class BillsHandlerSettings {
 	}
 
 	/**
-	 * Specifies how the driver expects combi-deals to be sent from POS 
-	 * @param consolidatedCombiDealsExpected true to send combi-deals consolidated 
+	 * Specifies how the driver expects combi-deals to be sent from POS
+	 *
+	 * @param consolidatedCombiDealsExpected true to send combi-deals consolidated
 	 */
 	public void setConsolidatedCombiDealsExpected(boolean consolidatedCombiDealsExpected) {
 		this.consolidatedCombiDealsExpected = consolidatedCombiDealsExpected;
 	}
 
+	/**
+	 * @return Returns timeout in milliseconds telling how long unTill waits for
+	 * an answer from driver before it shows "Timeout" error. Default timeout
+	 * is 50000.
+	 */
+	public int getWaitTimeout() {
+		return waitTimeout;
+	}
 
+	/**
+	 * Sets timeout telling how long unTill waits for an answer
+	 * from driver before it shows "Timeout" error
+	 *
+	 * @param waitTimeout Timeout, in milliseconds
+	 */
+	public void setWaitTimeout(int waitTimeout) {
+		this.waitTimeout = waitTimeout;
+	}
 }
