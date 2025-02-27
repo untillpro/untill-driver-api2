9e70b3c17af402df4e94d9c032067620c0467402
sda <d.solovyov@dev.untill.com>
2025-02-27 14:08:16 +0100

    Add BILL_EXTRA_QRCODE in BillsHandlerResultFields https://dev.untill.com/projects/#!728661

--- src/main/java/com/untill/driver/interfaces/bills/BillsHandlerResultFields.java
+++ src/main/java/com/untill/driver/interfaces/bills/BillsHandlerResultFields.java
@@ -4,9 +4,7 @@ package com.untill.driver.interfaces.bills;
  * Keys for the data in {@link BillsHandlerResult}.data property
  */
 public abstract class BillsHandlerResultFields {
-
 	public static final String PROFORMA_EXTRA_POS_CODE = "ProformaExtraData.posReference";
-
 	public static final String PROFORMA_EXTRA_BARCODE = "ProformaExtraData.barcode";
-
+	public static final String BILL_EXTRA_QRCODE = "$BillQRCode";
 }
