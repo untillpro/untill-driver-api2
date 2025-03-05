3c16cbe866e149018949d2a0e79cf3dc288f0d8e
Daniil Solovyov <solovyov.daniil@gmail.com>
2025-03-05 15:51:59 +0100

    Add billCopy to BillPrintingRequest (#126)
    https://dev.untill.com/projects/#!729072
    
    Co-authored-by: sda <d.solovyov@dev.untill.com>
    (cherry picked from commit c123d2c326a174be7cd2a149601deb5f7ab314a4)

--- src/main/java/com/untill/driver/interfaces/bills/BillPrintingRequest.java
+++ src/main/java/com/untill/driver/interfaces/bills/BillPrintingRequest.java
@@ -2,6 +2,7 @@ package com.untill.driver.interfaces.bills;
 
 public class BillPrintingRequest extends BillsHandlerRequest {
 	private String html;
+	private boolean billCopy;
 
 	public String getHtml() {
 		return html;
@@ -9,5 +10,13 @@ public class BillPrintingRequest extends BillsHandlerRequest {
 
 	public void setHtml(String html) {
 		this.html = html;
+	}
+
+	public boolean isBillCopy() {
+		return billCopy;
+	}
+
+	public void setBillCopy(boolean billCopy) {
+		this.billCopy = billCopy;
 	}
 }
