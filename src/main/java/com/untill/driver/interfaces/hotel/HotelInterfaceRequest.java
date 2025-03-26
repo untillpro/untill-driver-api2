36c498d91bd8b022af5bded1e8db418c0acad057
sda <d.solovyov@dev.untill.com>
2025-03-26 09:57:48 +0100

    Add user role restaurant Add waiter ID to hotel request Add driver progress query types https://dev.untill.com/projects/#!731452

--- src/main/java/com/untill/driver/interfaces/hotel/HotelInterfaceRequest.java
+++ src/main/java/com/untill/driver/interfaces/hotel/HotelInterfaceRequest.java
@@ -4,8 +4,22 @@ import com.untill.driver.interfaces.DriverRequest;
 
 /**
  * Basic class for {@link IHotelInterface} methods requests
- *
  */
 public class HotelInterfaceRequest extends DriverRequest {
+	private Long waiterId;
 
+	public Long getWaiterId() {
+		return waiterId;
+	}
+
+	public void setWaiterId(Long waiterId) {
+		this.waiterId = waiterId;
+	}
+
+	@Override
+	public String toString() {
+		return "HotelInterfaceRequest{"
+				+ "waiterId=" + waiterId
+				+ "} " + super.toString();
+	}
 }
