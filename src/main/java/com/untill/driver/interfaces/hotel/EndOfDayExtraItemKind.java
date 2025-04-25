c7f410fea3ea62e7e360777d184c79508cb62f0c
sda <d.solovyov@dev.untill.com>
2025-04-25 14:36:20 +0200

    EFT added tips constant https://dev.untill.com/projects/#!734543

--- src/main/java/com/untill/driver/interfaces/hotel/EndOfDayExtraItemKind.java
+++ src/main/java/com/untill/driver/interfaces/hotel/EndOfDayExtraItemKind.java
@@ -4,5 +4,12 @@ public final class EndOfDayExtraItemKind {
 	private EndOfDayExtraItemKind() {
 	}
 
+	/**
+	 * Total tips over the period
+	 */
 	public static final String TIP = "tip";
+	/**
+	 * Total EFT added tips over the period
+	 */
+	public static final String EFT_ADDED_TIPS = "eft_added_tips";
 }
