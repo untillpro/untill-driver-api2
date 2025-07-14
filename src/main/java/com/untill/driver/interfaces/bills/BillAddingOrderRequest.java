9232a52341ac791ca6ec2aecf973aea6b877bde4
sda <d.solovyov@dev.untill.com>
2025-07-14 14:57:36 +0200

    Add field reservationId https://dev.untill.com/projects/#!741534

--- src/main/java/com/untill/driver/interfaces/bills/BillAddingOrderRequest.java
+++ src/main/java/com/untill/driver/interfaces/bills/BillAddingOrderRequest.java
@@ -7,11 +7,14 @@ package com.untill.driver.interfaces.bills;
  * @see BillsHandlerRequest
  */
 public class BillAddingOrderRequest extends BillsHandlerRequest {
-
 	/**
 	 * Bill's order
 	 */
 	private Order order;
+	/**
+	 * When the bill has associated reservation, this field is filled otherwise it is <tt>null</tt>
+	 */
+	private Long reservationId;
 
 	public Order getOrder() {
 		return order;
@@ -21,10 +24,19 @@ public class BillAddingOrderRequest extends BillsHandlerRequest {
 		this.order = order;
 	}
 
+	public Long getReservationId() {
+		return reservationId;
+	}
+
+	public void setReservationId(Long reservationId) {
+		this.reservationId = reservationId;
+	}
+
 	@Override
 	public String toString() {
 		return "BillAddingOrderRequest{"
 				+ "order=" + order
+				+ ", reservationId=" + reservationId
 				+ "} " + super.toString();
 	}
 }
