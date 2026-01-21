b57776e4922609bf1792043d7f6b66a881e4917f
sda <d.solovyov@dev.untill.com>
2026-01-21 15:22:58 +0100

    Add customer display interface https://dev.untill.com/projects/#!760868
    (cherry picked from commit c398bdd2222c276995a48f459eeb8f0cf65e280d)

--- src/main/java/com/untill/driver/interfaces/customerdisplay/ICustomerDisplay.java
+++ src/main/java/com/untill/driver/interfaces/customerdisplay/ICustomerDisplay.java
@@ -2,6 +2,7 @@ package com.untill.driver.interfaces.customerdisplay;
 
 import com.untill.driver.IDriver;
 import com.untill.driver.interfaces.IDriverInterface;
+import com.untill.driver.params.DriverConfiguration;
 
 /**
  * Interface for a customer display driver
@@ -13,19 +14,23 @@ public interface ICustomerDisplay extends IDriverInterface {
 	/**
 	 * Called when customer display is added to the order to display the order details
 	 *
+	 * @param cfg   - driver configuration
 	 * @param order - order to display
 	 */
-	void displayOrder(DisplayOrder order);
+	void displayOrder(DriverConfiguration cfg, DisplayOrder order);
 
 	/**
 	 * Called when the payment screen is shown to display the bill QR code
 	 *
+	 * @param cfg  - driver configuration
 	 * @param bill - bill to display
 	 */
-	void displayBill(DisplayBill bill);
+	void displayBill(DriverConfiguration cfg, DisplayBill bill);
 
 	/**
 	 * Displays the standby screen
+	 *
+	 * @param cfg - driver configuration
 	 */
-	void displayStandby();
+	void displayStandby(DriverConfiguration cfg);
 }
