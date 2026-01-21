de94a43f82e31406480c0f81f197f2b982430748
sda <d.solovyov@dev.untill.com>
2026-01-21 14:45:11 +0100

    Add customer display interface https://dev.untill.com/projects/#!760868
    (cherry picked from commit afad9b49bf5b170487f66fc6abd787c87e6f2084)

---
+++ src/main/java/com/untill/driver/interfaces/customerdisplay/DisplayOrderItem.java
@@ -0,0 +1,47 @@
+package com.untill.driver.interfaces.customerdisplay;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisplayOrderItem {
+	private int quantity;
+	private BigDecimal singlePrice;
+	private String name;
+	/**
+	 * Child items, optional
+	 */
+	private List<DisplayOrderItem> item = new ArrayList<>();
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getSinglePrice() {
+		return singlePrice;
+	}
+
+	public void setSinglePrice(BigDecimal singlePrice) {
+		this.singlePrice = singlePrice;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<DisplayOrderItem> getItem() {
+		return item;
+	}
+
+	public void setItem(List<DisplayOrderItem> item) {
+		this.item = item;
+	}
+}
